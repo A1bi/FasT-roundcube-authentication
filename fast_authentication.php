619d90cfd89943ac2f700b76d443af1310ef2a41
Albrecht Oster <albrecht@oster.online>
2020-08-09 14:00:04 +0200

    add link to authenticate below login form

--- fast_authentication.php
+++ fast_authentication.php
@@ -12,15 +12,18 @@
 class fast_authentication extends rcube_plugin
 {
   public $task = 'login';
+  private $rc;
 
   function init()
   {
+    $this->rc = rcube::get_instance();
+
     $this->load_config();
 
-    $rcmail = rcmail::get_instance();
-    $config = $rcmail->config;
+    $this->add_texts('i18n/');
 
     $this->add_hook('authenticate', array($this, 'authenticate'));
+    $this->add_hook('template_object_loginform', array($this, 'append_fast_auth_link'));
   }
 
   function authenticate($params)
@@ -28,6 +31,25 @@ class fast_authentication extends rcube_plugin
     if ($this->get_auth_code() == 'foo') {
       $params['user'] = 'foobar@a0s.de';
       $params['pass'] = 'foobar';
+    }
+
+    return $params;
+  }
+
+  function append_fast_auth_link($params) {
+    $url = $this->rc->config->get('fast_authentication_authcode_url');
+
+    if (isset($url)) {
+      $params['content'] .=
+        html::p('my-5',
+          html::a(
+            array(
+              'href' => $url,
+              'class' => 'btn btn-lg btn-primary w-100'
+            ),
+            rcube::Q($this->gettext('auth_link_text'))
+          )
+        );
     }
 
     return $params;
